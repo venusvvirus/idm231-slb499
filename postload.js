85f8730fc8ff5814b05ccb0b09b784211a80ae17
venusvvirus <slb499@drexel.edu>
2025-01-30 14:32:21 -0500

    Add files via upload

---
+++ postload.js
@@ -0,0 +1,22 @@
+//
+// My first JS code below
+//
+
+console.log('Hello World!');
+let fname = 'Saffron';
+let age = 20;
+const pie = 3.1415926; 
+//
+console.log('Well, well, well ' + fname + ' is ' + age + ' years old');
+
+let greeting = `Hello ${fname},
+
+you are ${age} years old`;
+console.log(greeting);
+
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log('Array value is: ' + daysOfWeek);
+console.log('Day of rest is: ' + daysOfWeek[6]);
+
+let tDate = new Date();
+console.log('Today\'s date object is: ' + tDate);
